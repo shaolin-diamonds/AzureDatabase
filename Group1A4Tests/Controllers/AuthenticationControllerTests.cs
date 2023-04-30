0ea03aa8d09ae707f7f34815be171b209682e299
shaolin-diamonds <crios0718@gmail.com>
2023-04-30 16:54:53 -0500

    Update with unit tests

---
+++ Group1A4Tests/Controllers/AuthenticationControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group1A4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Group1A4.Controllers;
+
+namespace Group1A4.Tests
+{
+    [TestClass()]
+    public class AuthenticationControllerTests
+    {
+        [TestMethod()]
+        public void AuthenticateTest()
+        {
+            AuthenticationController controller = new AuthenticationController("fakekeynotlogin1111!");
+
+            User user = new User
+            {
+                username = "testusername",
+                password = "testpassword!!!"
+            };
+
+            var ret = controller.Authenticate(user.username, user.password);
+
+            Assert.IsNull(ret);
+        }
+    }
+}
---
+++ Group1A4Tests/Controllers/CoursesControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group1A4.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group1A4.Controllers.Tests
+{
+    [TestClass()]
+    public class CoursesControllerTests
+    {
+        [TestMethod()]
+        public void AuthUserTest()
+        {
+            AuthenticationController controller = new AuthenticationController("fakekeynotlogin1111!");
+
+            User user = new User
+            {
+                username = "testusername",
+                password = "testpassword!!!"
+            };
+
+            var ret = controller.Authenticate(user.username, user.password);
+
+            Assert.IsNull(ret);
+        }
+    }
+}
